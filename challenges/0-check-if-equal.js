cb1574a39fea0e36adc0862af42007c7223a3a3c
Sgnahore <stephaniergnahore@gmail.com>
2023-06-18 21:07:43 +0100

    no comments added, basic function sorted

--- challenges/0-check-if-equal.js
+++ challenges/0-check-if-equal.js
@@ -1,11 +1,17 @@
-function isEqual(numOne === numTwo) {
-    return True
+function isEqual(numOne, numTwo) {
+    if (numOne === numTwo) {
+    return true;
+
     
 } else {
-    return False
+    return false;
+}
 }
 
-//test cases
 
 // test cases
 console.log("isEqual(4, 4):", isEqual(4, 4), "should be True");
+console.log("isEqual(0, 0):", isEqual(0, 0), "should be True");
+console.log("isEqual(-4, -4):",isEqual(-4, -4), "should be True");
+console.log("isEqual(4, -4):", isEqual(4, -4), "should be False");
+console.log("isEqual(0, 1):", isEqual(0, 1), "should be False");
