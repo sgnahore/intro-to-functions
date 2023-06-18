a498c88ed486c532db9ab1455b31272a17143c43
Sgnahore <stephaniergnahore@gmail.com>
2023-06-18 20:47:52 +0100

    Version without comments

---
+++ challenges/0-check-if-equal.js
@@ -0,0 +1,11 @@
+function isEqual(numOne === numTwo) {
+    return True
+    
+} else {
+    return False
+}
+
+//test cases
+
+// test cases
+console.log("isEqual(4, 4):", isEqual(4, 4), "should be True");
